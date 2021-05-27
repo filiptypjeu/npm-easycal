a683796521803dd94f5226a21997ec66e6cda28a
Filip Stenbacka <filip.stenbacka@gmail.com>
2021-05-28 02:37:39 +0300

    Added tests.

--- src/__tests__/easycal.test.ts
+++ src/__tests__/easycal.test.ts
@@ -1,9 +1,29 @@
 import { getEvents } from "../index";
 import { url } from "./variables/easycal.test_variables";
 
-test("events", async () => {
-  const res = await getEvents(url);
-  console.log(res.past.map(e => e.summary));
-  console.log(res.ongoing.map(e => e.summary));
-  console.log(res.future.map(e => e.summary));
+test("no url", async () => {
+  expect(getEvents("url")).rejects.toBeDefined();
 });
+
+test("invalid url", async () => {
+  const res = await getEvents("https://calendar.google.com/calendar");
+  expect(res.future).toBeDefined();
+  expect(res.past).toBeDefined();
+  expect(res.ongoing).toBeDefined();
+  expect(res.invalid).toBeDefined();
+
+  expect(res.future).toHaveLength(0);
+  expect(res.past).toHaveLength(0);
+  expect(res.ongoing).toHaveLength(0);
+  expect(res.invalid).toHaveLength(0);
+});
+
+if (url) {
+  test("url", async () => {
+    const res = await getEvents(url);
+    expect(res.future).toBeDefined();
+    expect(res.past).toBeDefined();
+    expect(res.ongoing).toBeDefined();
+    expect(res.invalid).toBeDefined();
+  });
+}
